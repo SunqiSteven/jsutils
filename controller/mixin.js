de9b9f0b2cf68fcff96fda32afbdc8683b7e14b9
steven <sunqi_925@163.com>
2016-01-01 21:10:50 +0800

    123

--- controller/mixin.js
+++ controller/mixin.js
@@ -1,1 +1,20 @@
-mixin.js
+window.mixin = function(){};
+window.mixin.prototype={
+	   serialize:function(){
+
+	   }
+}
+if(window.utils === undefined){ window.utils = {}}
+window.utils.augment(recevingClass,givingClass){
+	if(arguments[2]){
+    	for(var i=2,len=arguments.length;i<len;i++){
+        	recevingClass.prototype[arguments[i]] = givingClass.prototype[arguments[i]];
+    	}
+	}else{
+	    for( method in givingClass){
+	    	if(!recevingClass.prototype[method]){
+	    		  recevingClass.prototype[method]=givingClass.prototype[method];
+	    	}
+	    }
+	}
+}
