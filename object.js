41cb22e516c38e5f20a7929cf4cc54e2386e8987
sunqisteven <sunqi_925@163.com>
2023-03-16 15:29:10 +0800

    Update object.js

--- object.js
+++ object.js
@@ -16,3 +16,29 @@ window.apisClasses.section.prototype.getCurSection = function(){
 window.apisClasses.section.prototype.getRankInfo   = function(){
 	return {"data":[{}]}
 };
+
+let totalPages = Math.ceil(total/(pageParams.pageSize))
+  let pagesArr = []
+  if (pageParams.current >= 5) {
+    let base = pageParams.current - 2
+    if (pageParams.current + 2 > totalPages) {
+      base = totalPages - 4
+    }
+    pagesArr = (new Array(5)).fill(0).map((t,i) => i+ base)
+  } else {
+    pagesArr = (new Array(5)).fill(0).map((t,i) => i+ 1)
+  }
+  if (pagesArr[0] !== 1) {
+    pagesArr.unshift(1)
+  }
+  
+  if (pagesArr[pagesArr.length - 1] !== totalPages) {
+    pagesArr.push(totalPages)
+  }
+  if (pagesArr[0] && pagesArr[1] && pagesArr[1] - pagesArr[0] > 1) {
+    pagesArr.splice(1,0,'...')
+  }
+  const pagesArrLen = pagesArr.length
+  if (pagesArr[pagesArrLen - 1] && pagesArr[pagesArrLen - 2] && pagesArr[pagesArrLen - 1] - pagesArr[pagesArrLen - 2] > 1) {
+    pagesArr.splice(pagesArrLen - 1,0,'...')
+  }
